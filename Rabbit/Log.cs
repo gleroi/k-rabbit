c7b15b707e246c11ac33c18d1b8fffe033866f34
Guillaume Leroi <guillaume-externe.leroi@erdf.fr>
2015-06-19 17:47:32 +0200

    initial commit

---
+++ Rabbit/Log.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Rabbit
+{
+    static class Log
+    {
+        public static void Write(string message)
+        {
+            Debug.WriteLine(message);
+        }
+
+        public static void Write(string format, params object[] parameters)
+        {
+            Debug.WriteLine(String.Format(format, parameters));
+        }
+    }
+}
