7a10d3791fed37a19e54aa6726a528e08078d925
Guillaume Leroi <guillaume-externe.leroi@erdf.fr>
2015-07-01 16:58:30 +0200

    move with or without compteur scoring

---
+++ Rabbit.Tests/World/WorldSimulator_ScoreTests.cs
@@ -0,0 +1,100 @@
+﻿using Rabbit.World;
+using Xunit;
+
+namespace Rabbit.Tests.World
+{
+    public class WorldSimulator_ScoreTests
+    {
+        private WorldState GivenWorld(int x, int y, int cx, int cy)
+        {
+            var world = AWorld.GivenWorld()
+                .WithPlayer(x, y)
+                .WithCaddy(1, 1)
+                .WithCaddy(1, 3)
+                .WithCompteur(cx, cy);
+            world.Initialize();
+            return world;
+        }
+
+        [Theory,
+         InlineData(10, 9, Direction.E),
+         InlineData(8, 9, Direction.O),
+         InlineData(9, 8, Direction.N),
+         InlineData(9, 10, Direction.S)]
+        public void Move_ShouldNotChangeScore
+            (int x, int y, Direction direction)
+        {
+            var world = this.GivenWorld(9, 9, 0, 0);
+
+            APlayer.Is(world.Players[0], 9, 9, 0, PlayerState.Playing);
+            ACompteur.Is(world.Compteurs[0], 0, 0);
+            Assert.False(world.Players[0].HasCompteur);
+
+            var simulator = new WorldSimulator(world);
+            var nworld = simulator.ApplyAction(0, direction);
+
+            APlayer.Is(nworld.Players[0], x, y, 0, PlayerState.Playing);
+            ACompteur.Is(nworld.Compteurs[0], 0, 0);
+            Assert.False(nworld.Players[0].HasCompteur);
+        }
+
+        [Theory,
+         InlineData(8, 9, Direction.E),
+         InlineData(10, 9, Direction.O),
+         InlineData(9, 10, Direction.N),
+         InlineData(9, 8, Direction.S)]
+        public void MoveOnCompteur_ShouldIncreaseScore(int x, int y, Direction direction)
+        {
+            var world = this.GivenWorld(x, y, 9, 9);
+
+            APlayer.Is(world.Players[0], x, y, 0, PlayerState.Playing);
+            Assert.False(world.Players[0].HasCompteur);
+
+            var simulator = new WorldSimulator(world);
+            var nworld = simulator.ApplyAction(0, direction);
+
+            APlayer.Is(nworld.Players[0], 9, 9, 1, PlayerState.Playing);
+            Assert.True(nworld.Players[0].HasCompteur);
+        }
+
+        [Theory,
+         InlineData(10, 9, Direction.E),
+         InlineData(8, 9, Direction.O),
+         InlineData(9, 8, Direction.N),
+         InlineData(9, 10, Direction.S)]
+        public void MoveWithCompteur_ShouldNotChangeScore(int x, int y, Direction direction)
+        {
+            var world = this.GivenWorld(9, 9, 9, 9);
+
+            APlayer.Is(world.Players[0], 9, 9, 0, PlayerState.Playing);
+            ACompteur.Is(world.Compteurs[0], 9, 9);
+            Assert.True(world.Players[0].HasCompteur);
+
+            var simulator = new WorldSimulator(world);
+            var nworld = simulator.ApplyAction(0, direction);
+
+            APlayer.Is(nworld.Players[0], x, y, 0, PlayerState.Playing);
+            ACompteur.Is(nworld.Compteurs[0], x, y);
+            Assert.True(nworld.Players[0].HasCompteur);
+        }
+
+        [Theory,
+         InlineData(1, 2, Direction.S),
+         InlineData(0, 3, Direction.E),
+         InlineData(1, 4, Direction.N),
+         InlineData(2, 3, Direction.O)]
+        public void MoveOnOtherCaddyWithCompteur_ShouldNotChangeScore(int x, int y, Direction direction)
+        {
+            var world = this.GivenWorld(x, y, x, y);
+
+            APlayer.Is(world.Players[0], x, y, 0, PlayerState.Playing);
+            Assert.True(world.Players[0].HasCompteur);
+
+            var simulator = new WorldSimulator(world);
+            var nworld = simulator.ApplyAction(0, direction);
+
+            APlayer.Is(nworld.Players[0], 1, 3, 0, PlayerState.Playing);
+            Assert.True(nworld.Players[0].HasCompteur);
+        }
+    }
+}
--- Rabbit/World/WorldSimulator.cs
+++ Rabbit/World/WorldSimulator.cs
@@ -15,50 +15,62 @@ namespace Rabbit.World
         public WorldState ApplyAction(int iplayer, Direction direction)
         {
             Point npos = this.GetNewPos(iplayer, direction);
-
             var nplayers = new List<Player>(this.World.Players);
             var ncompteurs = new List<Compteur>(this.World.Compteurs);
+            WorldState newWorld = WorldState.Create(this.World.Round, nplayers, ncompteurs, this.World.Caddies);
 
             var player = this.World.Players[iplayer];
-
             if (this.IsValidMove(iplayer, npos))
             {
                 var nplayer = new Player(player.Id, npos.X, npos.Y, player.Score, PlayerState.Playing);
-                // compteur
-                if (player.HasCompteur)
-                {
-                    var home = this.World.Caddies[iplayer];
-                    if (nplayer.Pos == home.Pos)
-                    {
-                        nplayer.IncreaseScore(30);
-                        ncompteurs.Remove(ncompteurs[player.CompteurId]);
-                        this.World.CheckCompteur(ref nplayer);
-                    }
-                    else
-                    {
-                        ncompteurs[player.CompteurId] = new Compteur(npos.X, npos.Y);
-                        nplayer.UpdateCompteur(player.CompteurId);
-                    }
-                }
-                else
-                {
-                    this.World.CheckCompteur(ref nplayer);
-                    if (nplayer.HasCompteur)
-                    {
-                        nplayer.IncreaseScore(1);
-                    }
-                }
-                // baffe
-
-                nplayers[iplayer] = nplayer;
+                newWorld.Players[iplayer] = nplayer;
+                
+                this.HandleCompteurRules(newWorld, iplayer);
+                this.HandleBaffe(newWorld, iplayer);
             }
             else
             {
-                nplayers[iplayer] = new Player(iplayer, player.Pos.X, player.Pos.Y, player.Score - 5,
-                                               PlayerState.Stunned);
+                newWorld.Players[iplayer] = new Player(iplayer, player.Pos.X, player.Pos.Y, player.Score - 5,
+                                                       PlayerState.Stunned);
             }
-            this.World = WorldState.Create(this.World.Round, nplayers, ncompteurs, this.World.Caddies);
+            this.World = newWorld;
+
             return this.World;
+        }
+
+        private void HandleBaffe(WorldState newWorld, int iplayer)
+        {
+        }
+
+        private void  HandleCompteurRules(WorldState newWorld, int iplayer)
+        {
+            var player = this.World.Players[iplayer];
+            var nplayer = newWorld.Players[iplayer];
+
+            if (player.HasCompteur)
+            {
+                var home = newWorld.Caddies[iplayer];
+                if (nplayer.Pos == home.Pos)
+                {
+                    nplayer.IncreaseScore(30);
+                    newWorld.Compteurs.Remove(newWorld.Compteurs[player.CompteurId]);
+                    newWorld.CheckCompteur(ref nplayer);
+                }
+                else
+                {
+                    newWorld.Compteurs[player.CompteurId] = new Compteur(nplayer.Pos.X, nplayer.Pos.Y);
+                    nplayer.UpdateCompteur(player.CompteurId);
+                }
+            }
+            else
+            {
+                newWorld.CheckCompteur(ref nplayer);
+                if (nplayer.HasCompteur)
+                {
+                    nplayer.IncreaseScore(1);
+                }
+            }
+            newWorld.Players[iplayer] = nplayer;
         }
 
         private Point GetNewPos(int player, Direction direction)
